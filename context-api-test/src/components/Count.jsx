c9a14c50a5e5e31bbd3d4b23891e3a038c0595ac
Mehedi Hasan <mehedihasansumit1@gmail.com>
2024-11-30 12:25:08 +0600

    array with memo test

--- context-api-test/src/components/Count.jsx
+++ context-api-test/src/components/Count.jsx
@@ -2,22 +2,27 @@ import { useContext } from "react";
 import { CountContext } from "../contexts/CountContext";
 import Dummy from "./Dummy";
 import UseMemoDummy from "./UseMemoDummy";
+import ArrMemoDummy from "./ArrMemoDummy";
 
 const Count = () => {
 
-    const { intialCount, count, onAddCount, onAddInitialCount } = useContext(CountContext);
+    const { intialCount, count, arrCount, onAddInitialCount, onAddCount, onAddArrCount } = useContext(CountContext);
     console.log('rendered');
 
     return (
-        <div style={{display:"flex",gap:"10px"}}>
+        <div style={{ display: "flex", gap: "10px" }}>
             <button onClick={onAddCount}>
                 count is {count}
             </button>
             <button onClick={onAddInitialCount}>
                 intial count is {intialCount}
             </button>
+            <button onClick={onAddArrCount}>
+                Arr count is {arrCount}
+            </button>
             <Dummy intialCount={intialCount} />
             <UseMemoDummy />
+            <ArrMemoDummy arrCount={arrCount} />
         </div>
     )
 
--- context-api-test/src/contexts/CountContext.jsx
+++ context-api-test/src/contexts/CountContext.jsx
@@ -6,11 +6,13 @@ export const CountContext = createContext();
 const CountContextProvider = ({ children }) => {
     const [intialCount, setIntialCount] = useState(0);
     const [count, setCount] = useState(0);
+    const [arrCount, setArrCount] = useState([0]);
 
     const onAddCount = () => { setCount(count => count + 1) };
     const onAddInitialCount = () => { setIntialCount(count => count + 1) }
+    const onAddArrCount = () => { setArrCount(count => [...count, count.length]) }
 
-    return <CountContext.Provider value={{ intialCount, onAddInitialCount, count, onAddCount }}>
+    return <CountContext.Provider value={{ intialCount, onAddInitialCount, count, onAddCount, arrCount, onAddArrCount }}>
         {children}
     </CountContext.Provider>
 };
